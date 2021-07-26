83f97a7aace29e98b693ab2744940b822d24ab70
dijiflex <dijiflex@gmail.com>
2021-07-26 15:07:09 +0300

    implemented landmark detection

--- src/App.js
+++ src/App.js
@@ -5,13 +5,59 @@ import './App.css';
 
 
 import * as tf from '@tensorflow/tfjs';
-import * as facemesh from '@tensorflow-models/facemesh'
+import * as facemesh from "@tensorflow-models/face-landmarks-detection";
 import Webcam  from 'react-webcam';
 import userEvent from '@testing-library/user-event';
+import { drawMesh } from "./utilities";
 
 function App() {
     const webcamRef = useRef(null);
     const canvasRef = useRef(null);
+
+    //  Load posenet
+  const runFacemesh = async () => {
+   
+    // NEW MODEL
+    const net = await facemesh.load(facemesh.SupportedPackages.mediapipeFacemesh);
+    setInterval(() => {
+      detect(net);
+    }, 100);
+  };
+
+  const detect = async (net) => {
+    if (
+      typeof webcamRef.current !== "undefined" &&
+      webcamRef.current !== null &&
+      webcamRef.current.video.readyState === 4
+    ) {
+      // Get Video Properties
+      const video = webcamRef.current.video;
+      const videoWidth = webcamRef.current.video.videoWidth;
+      const videoHeight = webcamRef.current.video.videoHeight;
+
+      // Set video width
+      webcamRef.current.video.width = videoWidth;
+      webcamRef.current.video.height = videoHeight;
+
+      // Set canvas width
+      canvasRef.current.width = videoWidth;
+      canvasRef.current.height = videoHeight;
+
+      // Make Detections
+      // OLD MODEL
+      //       const face = await net.estimateFaces(video);
+      // NEW MODEL
+      const face = await net.estimateFaces({input:video});
+      console.log(face);
+
+      // Get canvas context
+      const ctx = canvasRef.current.getContext("2d");
+      requestAnimationFrame(()=>{drawMesh(face, ctx)});
+    }
+  };
+
+  runFacemesh();
+
   return (
     <div className="App">
       
